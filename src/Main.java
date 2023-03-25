ee056fe432c305b5b2adf964120fa47fed7b1aea
Ruan Cardozo <ruan.cardozo@catolicasc.edu.br>
2023-03-24 22:20:03 -0300

    adicionar classe endereco

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 	public class Main {
 		public static void main(String[] args) {
-			Endereco e = new Endereco()
+			Endereco e = new Endereco();
 			Pessoa p = new Pessoa("Ruan", "115.302.289-30");
 			p.setTelefone("+5547984676946");
-			System.out.println("Ola" + p.getNome());
+			e.setBairro("AMerica");
+			p.setEndereco(e);
+			System.out.println("Ola " + p.getNome() + " Bairro " + p.getEndereco().getBairro());
 		}
 	}
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -42,70 +42,11 @@ import java.util.Date;
 			Telefone = telefone;
 		}
 
-		public class Endereco {
-
-			public Endereco (String endereco, String complemento, String bairro, String cidade, String estado, String cep) {
-				this.setEndereco(endereco);
-				this.setComplemento(complemento);
-				this.setBairro(bairro);
-				this.setCidade(cidade);
-				this.setEstado(estado);
-				this.setCep(cep);
-			}
-
-			public String endereco;
-
-			public String getEndereco() {
-				return endereco;
-			}
-
-			public void setEndereco(String endereco) {
-				this.endereco = endereco;
-			}
-
-			public String getComplemento() {
-				return complemento;
-			}
-
-			public void setComplemento(String complemento) {
-				this.complemento = complemento;
-			}
-
-			public String getBairro() {
-				return bairro;
-			}
-
-			public void setBairro(String bairro) {
-				this.bairro = bairro;
-			}
-
-			public String getCidade() {
-				return cidade;
-			}
-
-			public void setCidade(String cidade) {
-				this.cidade = cidade;
-			}
-
-			public String getEstado() {
-				return estado;
-			}
-
-			public void setEstado(String estado) {
-				this.estado = estado;
-			}
-
-			public String getCep() {
-				return cep;
-			}
-
-			public void setCep(String cep) {
-				this.cep = cep;
-			}
-			String complemento;
-			String bairro;
-			String cidade;
-			String estado;
-			String cep;
+		public Endereco getEndereco() {
+			return endereco;
 		}
-}
+
+		public void setEndereco(Endereco endereco) {
+			this.endereco = endereco;
+		}
+	}
